537daee66cd9b949b01145748a81459d1ee30208
Nanna Berg <bergnm@hotmail.com>
2024-05-15 11:32:52 +0200

    added a DatabaseServices class with currently one public method to store a survey

---
+++ src/Model/Database/DatabaseServices.cs
@@ -0,0 +1,36 @@
+
+using System.Text.Json;
+namespace DatabaseServices;
+using Survey = Survey.Survey;
+using Result = Result.Result;
+public class DatabaseServices {
+
+    private string SurveyDatabasePath = "./surveyDatabase.json";
+    public DatabaseServices() {
+        if (!File.Exists(SurveyDatabasePath)) {
+            List<Survey> emptyList = new List<Survey>{};
+            string jsonString = JsonSerializer.Serialize(emptyList);
+            //ok for the json file with the empty list to be created upon instantiation of this class if it does not already exist?
+            File.WriteAllText(SurveyDatabasePath, jsonString);
+        }
+    }
+
+    public bool StoreSurvey(Survey survey) {
+        List<Survey> surveys = LoadAllSurveysFromDatabase();
+        //add check that this survey does not already exist? will surveys only ever be stored as complete finished surveys?
+        surveys.Add(survey);
+        SaveSurveysToFile(surveys);
+        return true;
+    }
+
+    private List<Survey> LoadAllSurveysFromDatabase() {
+        string jsonString = File.ReadAllText(SurveyDatabasePath);
+        List<Survey> surveys = JsonSerializer.Deserialize<List<Survey>>(jsonString)!;
+        return surveys;
+    }
+    private void SaveSurveysToFile(List<Survey> surveys) {
+        string jsonString = JsonSerializer.Serialize(surveys);
+        File.WriteAllText(SurveyDatabasePath, jsonString);
+    }
+}
+
--- src/Model/DatabaseAPI/IDatabase.cs
+++ src/Model/DatabaseAPI/IDatabase.cs
@@ -1,14 +1,23 @@
+// using FrontEndAPI;
+// using Survey;
+// using Result;
 using FrontEndAPI;
-using Survey;
-using Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Backend.DatabaseAPI;
 
+using Survey = Survey.Survey;
+using Result = Result.Result;
 public interface IDatabase {
     int GetNextSurveyID();
-    bool StoreSurvey(Survey);
-    Survey GetSurvey(SurveyId);
-    bool ExportSurvey(Id,Path);
-    bool ImportSurvey(Path);
-    Result[] GetResults(Id);
-    bool StoreResults(Id, QuestionId, UserId, Results);
+    bool StoreSurvey(Survey survey);
+    Survey GetSurvey(int surveyId);
+    bool ExportSurvey(int id,string path);
+    bool ImportSurvey(string path);
+    List<Result> GetResults(int id);
+    bool StoreResults(int id, int questionId, int userId, List<Result> results);
 }
