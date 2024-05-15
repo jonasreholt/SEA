02bc266f26065dcaa8d4815ca75a10c4fff45406
jonasreholt <jsf130@alumni.ku.dk>
2024-05-15 10:51:54 +0200

    Make default login for use during development

--- src/Model/FrontEndAPI/FrontEndMainMenu.cs
+++ src/Model/FrontEndAPI/FrontEndMainMenu.cs
@@ -1,11 +1,31 @@
 namespace FrontEndAPI;
-using Survey;
 
-    public static class FrontEndMainMenu {
-        public static bool ValidateSuperUser(string username, string password) {
-            return false;
-        }
-        public static IGetSurvey? GetSurvey(int surveyId) {
-            return null;
-        }
+public static class FrontEndMainMenu
+{
+    private const string DeveloperUserName = "10xdeveloper";
+    private const string DeveloperPassword = "ornot";
+    private const int DeveloperPin = 123456;
+
+    public static bool ValidateSuperUser(string username, string password) {
+        return username == DeveloperUserName && password == DeveloperPassword;
     }
+    public static IGetSurvey? GetSurvey(int surveyId)
+    {
+        return surveyId == DeveloperPin
+            ? new MockSurvey()
+            : null;
+    }
+
+    private class MockSurvey : IGetSurvey
+    {
+        public Question.IGetQuestion GetNextQuestion {get;}
+
+        public Question.IGetQuestion GetQuestionA {get;}
+
+        public Question.IGetQuestion GetQuestionB {get;}
+
+        public Answer.IGetAnswer GetAnswer {get;}
+
+        public Question.IGetQuestion GetPreviousQuestion {get;}
+    }
+}
