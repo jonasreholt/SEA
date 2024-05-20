47a2d05aa2922b714e1145cfefc63f846d9d2347
jonasreholt <jsf130@alumni.ku.dk>
2024-05-20 17:00:53 +0200

    Implement with new interfaces

---
+++ src/scivu/scivu/Model/SharedConstants.cs
@@ -0,0 +1,6 @@
+namespace scivu.Model;
+
+public static class SharedConstants
+{
+    public const int ScaleMinimumValue = 1;
+}
--- src/scivu/scivu/ViewModels/ScaleQuestionViewModel.cs
+++ src/scivu/scivu/ViewModels/ScaleQuestionViewModel.cs
@@ -1,30 +1,74 @@
+using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using Avalonia.Media.Imaging;
-using Question;
+using Model.Answer;
+using Model.Question;
+using ReactiveUI;
 using scivu.Model;
 
 namespace scivu.ViewModels;
 
 public class ScaleQuestionViewModel : ViewModelBase
 {
+    private static int _groupName;
+
+    private bool _foundImage = true;
+
+    public bool FoundImage
+    {
+        get => _foundImage;
+        private set => this.RaiseAndSetIfChanged(ref _foundImage, value);
+    }
     public Bitmap? Image { get; }
     public string Caption { get; }
     public string Text { get; }
 
+
+
     public ObservableCollection<ScaleViewModel> Buttons { get; } = new();
 
-    public ScaleQuestionViewModel(IGetAvaloniaQuestion question, ScaleViewModel[] buttons)
+    public ScaleQuestionViewModel(IReadOnlyQuestion question)
     {
-        Debug.Assert(question.GetType == QuestionType.Question,
-            "Handling multiple questions should not be done at this level");
-
-        Image = question.GetPicture;
-        Caption = question.GetCaption;
-        Text = question.GetText;
-        foreach (var button in buttons)
+        if (!string.IsNullOrEmpty(question.ReadOnlyPicture))
         {
-            Buttons.Add(button);
+            if (File.Exists(question.ReadOnlyPicture))
+            {
+                Image = new Bitmap(question.ReadOnlyPicture);
+            }
+            else
+            {
+                Debug.WriteLine($"Could not find file `{question.ReadOnlyPicture}`");
+
+                // Display Debug image
+                FoundImage = false;
+
+                Image = null;
+            }
+        }
+        else
+        {
+            Image = null;
+        }
+
+        Caption = question.ReadOnlyCaption;
+        Text = question.ReadOnlyText;
+
+        var answer = question.ReadOnlyAnswer;
+        Debug.Assert(answer.ReadOnlyAnswerType == AnswerType.Scale);
+        var answers = answer.ReadOnlyAnswers;
+        Debug.Assert(answers.Count == 2);
+
+        if (!Int32.TryParse(answers[0], out var min)) throw new ArgumentException("Minimum value");
+        if (!Int32.TryParse(answers[1], out var max)) throw new ArgumentException("Max value");
+
+        Debug.Assert(SharedConstants.ScaleMinimumValue <= min && min < max);
+        _groupName++;
+        for (; min <= max; min++)
+        {
+            var svm = new ScaleViewModel(_groupName.ToString(), min.ToString());
+            Buttons.Add(svm);
         }
     }
 }
