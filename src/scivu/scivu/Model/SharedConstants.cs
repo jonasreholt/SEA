10c2cf336d4864d811296d9fc5949572e4be4392
Nicolaj <nicolajrjensen@gmail.com>
2024-05-23 18:19:37 +0200

    Initial work on pause menu

--- src/scivu/scivu/Model/SharedConstants.cs
+++ src/scivu/scivu/Model/SharedConstants.cs
@@ -1,7 +1,9 @@
 namespace scivu.Model;
 
+
 public static class SharedConstants
 {
     public const int ScaleMinimumValue = 1;
     public const int ScaleMaxRange = 8;
+    public const int PinCodeLength = 6;
 }
---
+++ src/scivu/scivu/ViewModels/PauseMenuViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Model.SharedConstants;
+
+
+namespace scivu;
+
+public class PauseMenuViewModel
+{
+
+    private readonly IReadOnlySurveyWrapper _survey;
+    private readonly Action<string, object> _changeViewCommand;
+    private string? _pincode;
+    private bool _isLoginEnabled;
+    private bool _isLoggedIn;
+
+    private IReadOnlySurveyWrapper _survey;
+
+    public PauseMenuViewModel(IReadOnlySurveyWrapper survey, Action<string, object> changeViewCommand)
+    {
+        _survey = survey;
+        _isLoggedIn = false;
+    }
+
+    public bool IsLoginEnabled
+    {
+        get => _isLoginEnabled;
+        set => this.RaiseAndSetIfChanged(ref _isLoginEnabled, value);
+    }
+
+    public string ErrorMessage
+    {
+        get => _errorMessage;
+        private set => this.RaiseAndSetIfChanged(ref _errorMessage, value);
+    }
+
+    public string? Pincode
+    {
+        get => _pincode;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _pincode, value);
+            IsLoginEnabled = EnableLoginButton();
+        }
+    }
+
+    private async void DoLogin()
+    {
+        Debug.Assert(!_isLoggedIn);
+
+        if (Int32.TryParse(Password, out var pin))
+        {
+            // Should manually check the pin of the survey rather than trying to get a new one and see if it exists.
+            var survey = FrontEndMainMenu.GetSurvey(pin);
+            if (survey != null)
+            {
+                _isLoggedIn = true;
+                return;
+            }
+
+            ErrorMessage = ErrorDiagnostics.GetErrorMessage(ErrorDiagnosticsID.ERR_PinCodeNotFound);
+        }
+    }
+}
