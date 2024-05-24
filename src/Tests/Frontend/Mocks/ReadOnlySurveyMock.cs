c2d93c17a6596f51b4427532f01387bbe92713e9
jonasreholt <jsf130@alumni.ku.dk>
2024-05-24 23:54:28 +0200

    Tests

---
+++ src/Tests/Frontend/Mocks/ReadOnlySurveyMock.cs
@@ -0,0 +1,30 @@
+namespace Tests.Frontend.Mocks;
+
+using Model.Survey;
+using Model.Question;
+
+public class ReadOnlySurveyMock : IReadOnlySurvey
+{
+    public List<List<IReadOnlyQuestion>> Questions;
+    private int i = -1;
+    public int SurveyId {get; set; }
+    public string SurveyName {get; set; }
+
+    public bool PreviousQuestionExist() => i > 0;
+
+    public bool NextQuestionExist() => i + 1 < Questions.Count;
+
+    public IEnumerable<IReadOnlyQuestion>? TryGetNextReadOnlyQuestion()
+        => i + 1 < Questions.Count ? Questions[++i] : null;
+
+    public IEnumerable<IReadOnlyQuestion>? TryGetPreviousReadOnlyQuestion()
+    {
+        if (i == Questions.Count)
+        {
+            i -= 2;
+            return Questions[i];
+        }
+
+        return i - 1 >= 0 ? Questions[--i] : null;
+    }
+}
---
+++ src/Tests/Frontend/Mocks/ReadOnlySurveyWrapperMock.cs
@@ -0,0 +1,23 @@
+namespace Tests.Frontend.Mocks;
+
+using Model.Survey;
+
+public class ReadOnlySurveyWrapperMock : IReadOnlySurveyWrapper
+{
+    private List<IReadOnlySurvey> _survey;
+    public IReadOnlySurvey TryGetReadOnlySurveyVersion(int index) => _survey[index];
+
+    public int GetVersionCount() => _survey.Count;
+
+    public string SurveyWrapperName{get;}
+
+    public ReadOnlySurveyWrapperMock(IReadOnlySurvey survey)
+    {
+        _survey = new List<IReadOnlySurvey> { survey };
+    }
+
+    public ReadOnlySurveyWrapperMock(List<IReadOnlySurvey> surveys)
+    {
+        _survey = surveys;
+    }
+}
