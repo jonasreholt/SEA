95c7d40016bd17d70bf51d547ee170974ab62a2c
jonasreholt <jsf130@alumni.ku.dk>
2024-05-27 12:28:12 +0200

    Extract file explorer functionality

--- src/scivu/scivu/ViewModels/SurveyModifyViewModel.cs
+++ src/scivu/scivu/ViewModels/SurveyModifyViewModel.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.IO;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Platform.Storage;
 using Model.Question;
-
+using scivu.Model;
 
 
 namespace scivu.ViewModels;
@@ -25,17 +18,7 @@ public class SurveyModifyViewModel : ViewModelBase
 
     public async void AddPicture(IModifyQuestion question)
     {
-        if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
-            desktop.MainWindow?.StorageProvider is not { } provider)
-            throw new NullReferenceException("Missing StorageProvider instance.");
-
-        var file = await provider.OpenFilePickerAsync(new FilePickerOpenOptions()
-        {
-            Title = "Open Text File",
-            AllowMultiple = false,
-            FileTypeFilter = [FilePickerFileTypes.ImageAll]
-        });
-
-        question.ModifyPicture = file?.Count >= 1 ? file[0].Path.ToString() : string.Empty;
+        var file = await FileExplorer.OpenImageAsync();
+        question.ModifyPicture = file?.Path.ToString() ?? string.Empty;
     }
 }
