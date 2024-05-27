1a0142472f380523c9829f569a10148ad9868a0c
jonasreholt <jsf130@alumni.ku.dk>
2024-05-27 12:43:21 +0200

    Import survey on main menu

--- src/scivu/scivu/Model/ErrorDiagnostics.cs
+++ src/scivu/scivu/Model/ErrorDiagnostics.cs
@@ -13,6 +13,8 @@ public static class ErrorDiagnostics
         Errors[ErrorDiagnosticsID.ERR_InvalidLogin] = "Invalid username or password";
         Errors[ErrorDiagnosticsID.ERR_ScaleRangeNotInt] = "Range must consist of integers";
         Errors[ErrorDiagnosticsID.ERR_ScaleRangeInvalid] = $"Range must be [a,b] where {SharedConstants.ScaleMinimumValue}<a<b";
+        Errors[ErrorDiagnosticsID.WAR_InvalidSurveyFileType] = $"Invalid file type for survey";
+        Errors[ErrorDiagnosticsID.WAR_CouldNotImportSurvey] = "Survey could not be imported";
     }
 
     public static string GetErrorMessage(ErrorDiagnosticsID id)
