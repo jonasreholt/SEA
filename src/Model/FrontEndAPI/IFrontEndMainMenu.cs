06b902f23b1fcba37c34bed7e717c2aba5236960
toppggg <70447358+toppggg@users.noreply.github.com>
2024-05-29 07:30:50 +0200

    Made SurveyWrapper internal, and changed returntype of ValidateSuperUser to return IModifySurveyWrapper rather than SurveyWrapper

--- src/Model/FrontEndAPI/IFrontEndMainMenu.cs
+++ src/Model/FrontEndAPI/IFrontEndMainMenu.cs
@@ -2,7 +2,7 @@ namespace Model.FrontEndAPI;
 using Model.Survey;
 
 public interface IFrontEndMainMenu {
-    List<SurveyWrapper>? ValidateSuperUser(string username, string password);
+    List<IModifySurveyWrapper>? ValidateSuperUser(string username, string password);
     bool ImportSurvey(string filePath);
 
     bool ExportResults(int surveyId, string folderPath);
